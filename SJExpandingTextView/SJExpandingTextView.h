f63aed80e744dcb2a0cac246bac3e0a43d76d82f
Hunter Dolan <hunterhdolan@gmail.com>
2013-01-12 15:19:47 -0600

    Update Header.

--- SJExpandingTextView/SJExpandingTextView.h
+++ SJExpandingTextView/SJExpandingTextView.h
@@ -5,9 +5,15 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface SJExpandingTextView : NSTextView
+@interface SJExpandingTextView : NSTextView {
+    id actionSender;
+}
 
 @property int minimumWidth;
 @property int heightMargin;
+
+@property SEL action;
+
+- (void)setAction:(SEL)action Sender:(id)sender;
 
 @end
